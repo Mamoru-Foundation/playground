04011ebe148d4d03a0e3a26e5e4e89bb2cdb6de2
Olexander Hofman <oh@mamoru.network>
2023-09-19 12:31:25 +0200

    fix agent

--- eth-vulneranle-contract/agent/src/index.ts
+++ eth-vulneranle-contract/agent/src/index.ts
@@ -1,24 +1,25 @@
 import {EvmCtx} from "@mamoru-ai/mamoru-evm-sdk-as/assembly"
 import {IncidentSeverity, parameter, report} from "@mamoru-ai/mamoru-sdk-as/assembly"
-import {bytesToHex} from "@mamoru-ai/mamoru-sdk-as/assembly/util"
 
 export function main(): void {
     const ctx = EvmCtx.load();
-    const contractAddress = parameter("contractAddress").asString()
-    const contractOwner = parameter("contractOwner").asString()
-    const methodCounterDecreased = parameter("methodCountDecrease").asString()
-    const methodCounterIncreased = parameter("methodCountIncrease").asString()
+    const contractAddress = parameter("contractAddress").asString().toLowerCase()
+    const contractOwner = parameter("contractOwner").asString().toLowerCase()
 
-    for (let i = 0; i < ctx.events.length; i++) {
-        const event = ctx.events[i];
-
-        if (event.address != contractAddress) {
+    for (let i = 0; i < ctx.callTraces.length; i++) {
+        const trace = ctx.callTraces[i];
+        const tx = ctx.txs[trace.txIndex]
+        if (trace.to.toLowerCase() != contractAddress) {
             continue;
         }
 
-        const tx = ctx.txs[event.txIndex]
         const senderAddress = tx.from;
-        if (bytesToHex(event.topic0) == methodCounterDecreased || bytesToHex(event.topic0) == methodCounterIncreased) {
+        // Search for increaseCounter and decreaseCounter functions
+        const methodIncrease = trace.input.parse("increaseCounter(uint256 amount)")
+        const methodDecrease = trace.input.parse("decreaseCounter(uint256 amount)")
+        // Check if the function is called
+        if (methodIncrease != null || methodDecrease != null) {
+            // Check if the function is called by the not owner
             if (senderAddress != contractOwner) {
                 return report(tx.txHash, IncidentSeverity.Alert, `Unexpected: the function is not called by the owner`, null, senderAddress);
             }
